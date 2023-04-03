d7eb34f9e03161df9971080ec9ce50ce2171daac
gix1382 <123644646+gix1382@users.noreply.github.com>
2023-04-03 15:47:49 +0200

    new php
    fichiers déplacés

--- index.php
+++ index.php
@@ -1,41 +1,61 @@
-class png {
-    // Properties
-    public $name;
-    public $armor;
-    public $life;
+<?php
+class Personnage {
+  // Properties
+  public $name;
+  public $armor;
+  public $life;
+  public $degat;
 
+  function __construct($name,$armor,$life,$degat){
+    $this->name=$name;
+    $this->armor=$armor;
+    $this->life=$life;
+    $this->degat=$degat;
+    
 
-    // Methods
-    function set_name($name) {
-    $this -> name = $name;
-}
-function get_name() {
-    return $this -> name;
-}
-function set_armor($armor) {
-    $this -> armor = $armor;
-}
-function get_armor($armor) {
-    $this -> armor = $armor;
-}
-function set_life($life) {
-    $this -> life = $life;
-}
-function get_life($life) {
-    $this -> life = $life;
-}
   }
 
-$hero = new Hero();
-$minion = new Minion();
-$lieutenantMinion = new LieutenantMinion();
-$chefMinion = new ChefMinion();
+  // Methods
+  public function set_name($name) {
+    $this->name = $name;
+  }
+  public function get_name() {
+    return $this->name;
+  }
+  public function set_armor($armor) {
+    $this->armor = $armor;
+  }
+  public function get_armor($armor) {
+    $this->armor = $armor;
+  }
+  public function set_life($life) {
+    $this->life = $life;
+  }
+  public function get_life($life) {
+    $this->life = $life;
+  }
+  public function set_degat($degat) {
+    $this->degat = $degat;
+  }
+  public function get_degat($degat) {
+    $this->degat = $degat;
+  }
+}
 
-$hero -> set_name('Link');
-$hero -> set_armor('20');
-  
-  
-  echo $apple -> get_name();
-  echo "<br>";
-  echo $banana -> get_name();
-  ?>
+
+$hero = new Personnage('Link', 20, 200, 15);
+$minion = new Personnage('Mokoblin','',10,10);
+$lieutenantMinion = new Personnage('Lézalfos','',30,30);
+$chefMinion = new Personnage('Moblin','',100,100);
+
+
+
+echo $hero->get_name();
+echo "<br>";
+echo $minion->get_name();
+echo "<br>";
+echo $lieutenantMinion->get_name();
+echo "<br>";
+echo $chefMinion->get_name();
+
+?>
